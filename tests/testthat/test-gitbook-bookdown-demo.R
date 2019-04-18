2d2a47288a7b60eb9fa033633c6c254bf96c83b6
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-04-18 11:43:41 +0800

    [skip ci] hello

---
+++ tests/testthat/test-gitbook-bookdown-demo.R
@@ -0,0 +1,13 @@
+testthat::context("Testing gitbook")
+setwd(here::here(''))  # workspace is reset per file
+
+
+testthat::test_that("Testing bookdown-demo", {
+    testthat::expect_true(T)
+    
+    setwd('data-raw/bookdown-demo')
+    bookdown::render_book('', make_gitbook('https://bookdown.org/rstudio/bookdown-demo/', '../_output-bookdown-demo.yaml'), output_dir = '../../tests/testthat/output/bookdown-demo', new_session = T)
+    file.remove('packages.bib')
+});
+
+
