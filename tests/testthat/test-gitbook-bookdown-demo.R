fc22f4436ea692d9470c74f2b4057acef14f0dff
Zhuoer Dong <dongzhuoer@mail.nankai.edu.cn>
2019-12-21 19:11:05 +0800

    new Travis

--- tests/testthat/test-gitbook-bookdown-demo.R
+++ tests/testthat/test-gitbook-bookdown-demo.R
@@ -1,13 +1,13 @@
 testthat::context("Testing gitbook")
-setwd(here::here(''))  # workspace is reset per file
+setwd(here::here(""))  # workspace is reset per file
 
 
 testthat::test_that("Testing bookdown-demo", {
     testthat::expect_true(T)
-    
-    setwd('data-raw/bookdown-demo')
-    bookdown::render_book('', make_gitbook('https://bookdown.org/rstudio/bookdown-demo/', '../_output-bookdown-demo.yaml'), output_dir = '../../tests/testthat/output/bookdown-demo', new_session = T)
-    file.remove('packages.bib')
+
+    setwd("data-raw/bookdown-demo")
+    bookdown::render_book("", make_gitbook("../_output/bookdown-demo.yml", "https://bookdown.org/rstudio/bookdown-demo/"), output_dir = "../../tests/testthat/output/bookdown-demo", new_session = T)
+    file.remove("packages.bib")
 });
 
 
--- tests/testthat/test-gitbook-r-ninja.R
+++ tests/testthat/test-gitbook-r-ninja.R
@@ -1,14 +1,14 @@
-testthat::context("Testing gitbook")
-setwd(here::here(''))  # workspace is reset per file
+# testthat::context("Testing gitbook")
+# setwd(here::here(""))  # workspace is reset per file
 
 
 
-testthat::test_that("Testing r-ninja", {
-    testthat::skip_if_not_installed('rmini')
-    
-    testthat::expect_true(T)
+# testthat::test_that("Testing r-ninja", {
+#     testthat::skip_if_not_installed("rmini")
 
-    setwd('data-raw/r-ninja')
-    bookdown::render_book('', make_gitbook('https://bookdown.org/yihui/r-ninja/', '../_output-r-ninja.yaml'), output_dir = '../../tests/testthat/output/r-ninja', new_session = T)
-    file.remove('r-ninja.rds')
-});
+#     testthat::expect_true(T)
+
+#     setwd("data-raw/r-ninja")
+#     bookdown::render_book("", make_gitbook("../_output/r-ninja.yml", "https://bookdown.org/yihui/r-ninja/"), output_dir = "../../tests/testthat/output/r-ninja", new_session = T)
+#     file.remove("r-ninja.rds")
+# });
